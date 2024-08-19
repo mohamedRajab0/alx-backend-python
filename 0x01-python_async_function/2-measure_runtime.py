ed6b86030e4cdfaab012fd68a654785d10353a80
mohamedRajab0 <elzimmohamed@gmail.com>
2024-08-19 18:06:55 +0300

    cheecker 4

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,6 +8,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n, max_delay):
+    """ takes in 2 int arguments (in this order): n and max_delay.
+    You will spawn wait_random n times with the specified max_delay.
+    """
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay)) 
     return time.perf_counter() - start_time
